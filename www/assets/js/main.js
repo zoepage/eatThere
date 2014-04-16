558a5c786b623365529d6552731f09bf3f8015b2
Dennis Wilson <dennis.wilson@abakia.de>
2014-04-16 14:45:51 +0200

    adds improved paging mechanism

--- www/assets/js/main.js
+++ www/assets/js/main.js
@@ -12,15 +12,116 @@ $(function() {
     var environment,
         logger,
         hoodie,
+        $stage,
+        views,
+        viewOrder,
+        startingViewName,
+        currentViewName,
         $eateryItem,
         $btn,
         $menu,
         $logo,
         $eatery,
         $addEatery,
-        $stage,
         $item,
         hammertime;
+
+    // init globals
+    
+    $stage        = $('#stage');
+    $viewsWrapper = $('#views-wrapper');
+
+    $eateryItems  = $('#eateries.list li')
+    $peopleItems  = $('#people.list li')
+
+    $btn          = $('#menu');
+    $menu         = $('menu ul');
+    $logo         = $('#logo');
+    $eatery       = $('#eatery');
+    $addEatery    = $('#addEatery');
+    $item         = $('.stage ul li');
+    hammertime    = $stage.hammer();
+
+    // view/ui configuration
+    
+    views = {
+        'people-view': $('#people-view'),
+        'main-view':   $('#main-view'),
+        'eatery-view': $('#eatery-view')
+    };
+
+    viewOrder = [
+        'people-view',
+        'main-view',
+        'eatery-view'
+    ];
+
+    startingViewName = 'main-view';
+
+    /* Helpers */
+
+    function showView(viewName) {
+        var $view,
+            index;
+
+        $view = views[viewName];
+        index = viewOrder.indexOf(viewName);
+
+        if(viewName != undefined && index > -1) {
+            var css = {
+                left:( ( -100 * index) + '%')
+            };
+
+            $viewsWrapper.css(css);
+            $stage.attr('class', viewName);
+
+            logger.debug('show ', viewName);
+        }
+    }
+
+    function swipeLeft() {
+        swipeToDirection(1);
+    }
+
+    function swipeRight() {
+        swipeToDirection(-1);
+    }
+
+    function swipeToDirection(direction) {
+        var minIndex,
+            maxIndex,
+            tempIndex,
+            nextIndex;
+
+        minIndex  = 0;
+        maxIndex  = viewOrder.length - 1;
+        tempIndex = viewOrder.indexOf(currentViewName);
+
+        // figure if stage is in a valid state
+        if(tempIndex > -1) {
+
+            // apply swipe direction to viewOrder index
+            tempIndex += direction;
+
+            // correct boundaries if necessary
+            if(tempIndex < minIndex) {
+                tempIndex = minIndex;
+            }
+            if(tempIndex > maxIndex) {
+                tempIndex = maxIndex;
+            }
+
+            // allow index change
+            nextIndex = tempIndex;
+        }
+
+        if(nextIndex !== undefined) {
+            // if a valid nextIndex is given, apply viewstate
+            currentViewName = viewOrder[nextIndex];
+            showView(currentViewName);
+        }
+    }
+
 
 // ---------- fcn - UI ------------------------------------
 // --------------------------------------------------------
@@ -35,88 +136,6 @@ $(function() {
 
         menu.toggleClass('hide');
     }
-
-    function animateBackground(direction) {
-        var wrapStates,
-            statePointer,
-            stateClass;
-
-        wrapStates = [
-            'people',
-            'startscreen',
-            'eatery',
-        ];
-
-        stateClass = $('#wrap').attr('class');
-        if(wrapStates.indexOf(stateClass) > -1) {
-            statePointer = wrapStates.indexOf(stateClass);
-        }
-
-        if(direction == 'left') {
-            statePointer--;
-            if(statePointer < 0) {
-                statePointer = 0;
-            }
-        } else if(direction == 'right') {
-            statePointer++;
-            if(statePointer > (wrapStates.length-1)) {
-                statePointer = wrapStates.length - 1;
-            }
-        }
-
-
-        $('#wrap').attr('class', wrapStates[statePointer]);
-    }
-
-    function changeStage(direction){
-        var active,
-            dirVar,
-            act;
-
-
-        animateBackground(direction);
-
-        active = $('.active');
-
-        if(direction == 'right') {
-            dirGo       = '-=100%';
-            dirThere    = '+=100%';
-            dirWhere    =  function nextStage(){
-                if(active.next('.stage').hasClass('stage')) {
-                    act = active.next(".stage");
-                } 
-                return act;
-            };
-        } else {
-            dirGo       = '+=100%';
-            dirThere    = '-=100%';
-            dirWhere    =  function prevStage(){
-                if(active.prev('.stage').hasClass('stage')) {
-                    act = active.prev(".stage");
-                } 
-                return act;
-            };
-        }
-
-        if(dirWhere() !== undefined){
-            active.animate({
-                left: dirGo,
-                right: 0
-            }, 200, function() {
-                    dirWhere().animate({
-                        right: dirThere,
-                        left: 0
-                    }, 400, function(){
-                        dirWhere().addClass('active');
-                        active.removeClass('active');  
-                    })
-            });
-        }
-
-       
-    }
-
-
 
     /** ============== List edit Events ============== */
 
@@ -172,67 +191,21 @@ $(function() {
         handleEateryTitleHold(evnt);
     }
 
+    function handleStageSwipeLeft(evnt) {
+        swipeLeft();
+    }
+
+    function handleStageSwipeRight(evnt) {
+        swipeRight();
+    }
+
     // ------- delete item -------
     // ******* @ToDo add delete of item in storage
     function deleteItem(){
         $(this).remove();
     }
 
-    function initBindings() {
-        logger.debug('\t--> initBindings');
-
-        // @TODO: implement event with hoodie actions
-         $eateryItem.hammer().on('tap', toggleItem);
-
-        $btn.bind('click', toggleMenu);
-
-
-
-        // ******* @ToDo add dragright / dragleft event for mobiel
-
-        hammertime.on("swipeleft", function(ev) {
-          changeStage('right');
-        });
-
-        hammertime.on("swiperight ", function(ev) {
-            changeStage('left');
-        });
-
-        if($logo != undefined) {
-          Hammer($logo).on('tap',function(){
-               if($eatery.hasClass('hide')){
-                    $eatery.fadeIn(500);
-                } else {
-                    $eatery.fadeOut(500);
-                }
-            });
-        } 
-
-        initIosBindings();
-    }
-
-    function initIosBindings() {
-        $(document).on('touchmove', function(evnt) {
-            evnt.preventDefault();
-        });
-    }
-
-
-    function initGlobals() {
-        logger.debug('\t--> initGlobals');
-
-        $eateryItem = $('#eateryView li ')
-
-        $btn       = $('#menu');
-        $menu      = $('menu ul');
-        $logo      = $('#logo');
-        $eatery    = $('#eatery');
-        $addEatery    = $('#addEatery');
-        $stage     = $('#wrap');
-        $item      = $('.stage ul li');
-        hammertime = $stage.hammer();
-    }
-
+    // setup routines
     function initLogger() {
         logger = {
             debug: function() {
@@ -246,13 +219,81 @@ $(function() {
         };
     }
 
+    function initBindings() {
+        logger.debug('\t--> initBindings');
+
+        // @TODO: implement event with hoodie actions
+         $eateryItems.hammer().on('tap', toggleItem);
+
+        $btn.bind('click', toggleMenu);
+
+        // ******* @ToDo add dragright / dragleft event for mobiel
+        hammertime.on("swipeleft",  handleStageSwipeLeft);
+        hammertime.on("swiperight", handleStageSwipeRight);
+
+        if($logo != undefined) {
+          Hammer($logo).on('tap',function(){
+               if($eatery.hasClass('hide')){
+                    $eatery.fadeIn(500);
+                } else {
+                    $eatery.fadeOut(500);
+                }
+            });
+        } 
+
+        initMobileBindings();
+    }
+
+    function initMobileBindings() {
+        logger.debug('\t--> initMobileBindings');
+
+        $(document).on('touchmove', function(evnt) {
+            evnt.preventDefault();
+        });
+    }
+
+    function initHoodieBindings() {
+        logger.debug('\t--> initHoodieBindings');
+
+        window.hoodie.account.on('signout', function (user) {
+            window.location.href = 'login.html';
+        });
+
+        window.hoodie.account.on('unauthenticated', function (user) {
+            window.location.href = 'login.html';
+        });
+
+        window.hoodie.account.on('signin', function (user) {
+            $('html').attr('data-hoodie-account-status', 'signedin');
+            $('.hoodie-accountbar').find('.hoodie-username').text(username).attr('color', 'red');
+            alert('DRIN');
+        });
+    }
+
+    function initUi() {
+        logger.debug('\t--> initUi');
+
+        var cssConfig;
+
+        viewOrder.forEach(function(viewName, idx, viewNames) {
+            views[viewName].css({
+                left:((100 * idx) + '%')
+            })
+        });
+
+        currentViewName = startingViewName;
+        showView(currentViewName);
+    }
+
     function startApp() {
         environment = ENV_DEV;
         initLogger();
 
         logger.debug('Starting eatThere');
-        initGlobals();
+
+        initUi();
         initBindings();
+        initHoodieBindings();
 
         $('body').on('click.hoodie.data-api', '[data-hoodie-action]', function(evnt) {
             // @TODO: extract this as a seperate event handler
@@ -268,28 +309,9 @@ $(function() {
                 eventHandlers[action](evnt);
             }
         });
-
-        window.hoodie.account.on('signout', function (user) {
-            window.location.href = 'login.html';
-        });
-
-        window.hoodie.account.on('unauthenticated', function (user) {
-            window.location.href = 'login.html';
-        });
-
-        window.hoodie.account.on('signin', function (user) {
-            $('html').attr('data-hoodie-account-status', 'signedin');
-            $('.hoodie-accountbar').find('.hoodie-username').text(username).attr('color', 'red');
-            alert('DRIN');
-        });
-
     }
 
-
-
     // pseudo main
-
     startApp();
-
 });
 
